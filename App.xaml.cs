1e40a6814fcd433603e93d2a969ed84efebd4239
mbudak42 <1mehmetyahya@gmail.com>
2025-12-17 17:37:15 +0300

    First_Commit

---
+++ App.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Article_Graph_Analysis_Application;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+}
+
